7eb8966073f6f96e44e77438e401e26216365134
HamzaYazidi27 <117764730+HamzaYazidi27@users.noreply.github.com>
2025-07-28 11:33:06 +0100

    Add stagemanager/login.php 1753698785499

--- stagemanager/login.php
+++ stagemanager/login.php
@@ -1,8 +1,4 @@
 <?php
-// Activer l'affichage des erreurs pour le débogage
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 require_once 'config/config.php';
 require_once 'includes/functions.php';
 
